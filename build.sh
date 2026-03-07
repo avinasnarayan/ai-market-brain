5a7188496f1089d803b9f6261a6fcc0542e36650
avinashnarayan <avinas.narayan@gmail.com>
2026-03-07 11:24:11 +0530

    install pytorch cpu correctly

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pip install --upgrade pip
-
-# Install CPU PyTorch
-pip install torch --index-url https://download.pytorch.org/whl/cpu
-
-# Install remaining dependencies
-pip install -r requirements.txt
