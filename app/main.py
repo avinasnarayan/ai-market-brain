f63aa4ca872991e8a44ff297f4c87102067b7b0a
avinashnarayan <avinas.narayan@gmail.com>
2026-03-13 13:54:29 +0530

    fix scheduler function

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes.news import router as news_router
 from app.workers.scheduler import start_scheduler
+import threading
 
 app = FastAPI()
 
@@ -12,14 +13,12 @@ def root():
     return {"message": "AI Market Brain API running"}
 
 # start scheduler when server starts
-import threading
-
 @app.on_event("startup")
 def startup_event():
     print("Starting background news scheduler...")
 
     def run_scheduler():
-        start_news_scheduler()
+        start_scheduler()
 
     thread = threading.Thread(target=run_scheduler)
     thread.daemon = True
